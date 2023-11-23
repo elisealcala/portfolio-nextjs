97013a643f179a833ae16e74f7896102a9ee9169
Elizabeth Alcalá <elizabethalcala28@gmail.com>
2023-11-23 18:47:16 -0500

    feat: add index page

--- src/app/(general)/layout.tsx
+++ src/app/(general)/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <>
       <Navbar />
-      <main className="flex min-h-screen flex-col lg:w-[960px] xl:w-[1024px] mx-auto mt-32 px-[24px] lg:px-0">
+      <main className="flex flex-col lg:w-[960px] xl:w-[1024px] mx-auto mt-32 px-[24px] lg:px-0">
         {children}
       </main>
     </>
--- src/app/(general)/page.tsx
+++ src/app/(general)/page.tsx
@@ -1,10 +1,42 @@
+import BlogCard from "@/components/blog-card";
+import Link from "next/link";
+import { getDocuments } from "outstatic/server";
 
+async function getData() {
+  const posts = getDocuments("posts", ["title", "description", "publishedAt", "slug"]);
 
-export default function Home() {
+  const sortedPosts = posts
+    .sort((a, b) => new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime())
+    .filter((_, i) => i < 4);
+
+  return sortedPosts;
+}
+
+export default async function Home() {
+  const posts = await getData();
 
   return (
-    <>
-      cosito
-    </>
-  )
+    <div className="max-w-[600px]">
+      <div className="text-3xl mb-3">
+        {`Hi, my name is Elizabeth Alcalá.`}
+        <br /> {`I'm a frontend developer.`}
+      </div>
+      <p className="mt-6">Check my latest posts.</p>
+      <div className="flex flex-col mt-4 mb-8">
+        {posts.map((post) => (
+          <div className="mb-[10px] mb-8" key={post.slug}>
+            <BlogCard
+              slug={post.slug}
+              title={post.title}
+              description={post.description}
+              date={post.publishedAt.toString()}
+            />
+          </div>
+        ))}
+        <Link href="/blog" className="text-sm underline mt-8">
+          View all
+        </Link>
+      </div>
+    </div>
+  );
 }
